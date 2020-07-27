cd02d173bc053bf2e592632c24f6c88b5ca9f8f9
Samuel Vitorio Lima <34277130+SamuelLima0610@users.noreply.github.com>
2020-07-27 15:24:29 -0300

    Concerto do erro
    Nas classes Join e OnlyComputation

--- Join.js
+++ Join.js
@@ -1,16 +1,31 @@
 class Join {
     static async join(data){
-        let join = await data.map( async (element) => {
+        let togheter = await data.map( (element) => {
             let newElement = element;
-            let student = await data.filter(object => object.matricula == element.matricula)
-            if(student[0].nota != undefined && student[1].nota != undefined) {
-                console.log('teste')
-                newElement.media = (parseFloat(student[0].nota) + parseFloat(student[1].nota))/2;
-                //console.log(newState.media)
+            let student = data.filter(object => object.matricula == element.matricula)
+            if(student[0].nota != undefined && student[1].nota != undefined){
+                newElement.media = (parseFloat(student[0].nota) + parseFloat(student[1].nota))/2
+            }
+            if(student[0].resultado != undefined && student[1].resultado != undefined ){
+                let aprovado = 0;
+                let reprovadoFalta = 0;
+                if(student[0].resultado === 'APROVADO POR MÉDIA FINAL' || student[0].resultado === 'APROVADO'){
+                    aprovado++ 
+                } 
+                if(student[1].resultado === 'APROVADO POR MÉDIA FINAL' || student[1].resultado === 'APROVADO'){
+                    aprovado++ 
+                } 
+                if(student[0].resultado === 'REPROVADO POR FALTA'){
+                    reprovadoFalta++ 
+                } 
+                if(student[1].resultado === 'APROVADO POR FALTA'){
+                    reprovadoFalta++ 
+                } 
+                newElement.reprovadoFalta = (reprovadoFalta / 2) * 100;
             }
             return newElement;
         });
-        return join;
+        return togheter;
     }
 }
 
